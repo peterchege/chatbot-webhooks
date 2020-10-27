32088453a97c9b9ce061a4901457d32905a5d14a
peter chege <peterchege442@gmail.com>
2020-10-27 23:36:26 +0300

    fuction setup

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const dfff = require('dialogflow-fulfillment')
 
 
 app.get('/', (req, res) => {
     res.send("we are live")
 });
 
+app.post('/', (req, res)) => {
+    const agent = new dfff.WebhookClient({
+        request: req,
+        response: res
+
+    });
+
+    function demo(agent) {
+        agent.add("sending response from webhook server")
+    }
+}
 
 app.listen(3333, () => console.log("Server is live at port 3333"))
